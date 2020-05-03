19203ad59a2349a3f7597b638a51e971f22266b7
Saddam-tech <salokhiddinov0727@gmail.com>
2020-05-04 06:30:15 +0900

    deleteCharHandler

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -5,7 +5,6 @@ import React from 'react';
 const Validation = (props) => {
     
     let validationMessage = 'Text long enough'
-    let person = null
 
     if (props.InputLength <= 5) {
         validationMessage = 'Text too short'
